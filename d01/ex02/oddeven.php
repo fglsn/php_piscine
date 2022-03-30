a0631f8c8a2f2e1701d27450e7c8921fb77af1ff
Ilona Shakurova <leverseau19@gmail.com>
2022-03-30 14:09:15 +0300

    Replace trim func in oddeven

--- d01/ex02/oddeven.php
+++ d01/ex02/oddeven.php
@@ -6,8 +6,7 @@
 	while ($input && !feof($input))
 	{
 		echo "Enter a number: ";
-		$n = trim(fgets($input));
-
+		$n = str_replace("\n", "", fgets($input));
 		if ($n) {
 			switch (n) {
 				case is_numeric($n) && $n % 2 == 0:
@@ -16,8 +15,11 @@
 				case is_numeric($n) && $n % 2 != 0:
 					echo "The number $n is odd\n";
 					break;
+				case $n == '';
+					echo "The number $n is odd\n";
+					break;
 				default:
-				echo "'$n' is not a number\n";
+					echo "'$n' is not a number\n";
 			}
 		}
 	}
