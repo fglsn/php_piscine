19ee907193e3d8815dcaa2ef3c6e4b70137c2094
Ilona Shakurova <leverseau19@gmail.com>
2022-03-30 12:28:50 +0300

    Add oddeven

--- d01/ex02/oddeven.php
+++ d01/ex02/oddeven.php
@@ -1,4 +1,25 @@
 #!/usr/bin/php
 <?php
-	
+
+	$input = fopen('php://stdin', 'r');
+
+	while ($input && !feof($input))
+	{
+		echo "Enter a number: ";
+		$n = trim(fgets($input));
+
+		if ($n) {
+			switch (n) {
+				case is_numeric($n) && $n % 2 == 0:
+					echo "The number $n is even\n";
+					break;
+				case is_numeric($n) && $n % 2 != 0:
+					echo "The number $n is odd\n";
+					break;
+				default:
+				echo "'$n' is not a number\n";
+			}
+		}
+	}
+	echo "\n";
 ?>
