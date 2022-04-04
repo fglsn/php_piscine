0db49baa34fbfc4ece8a62c470ab9d14e6e7607f
Ilona Shakurova <leverseau19@gmail.com>
2022-04-04 15:29:51 +0300

    Add test, comment testing lines, fix regex a bit by adding regex options

--- d02/ex02/magnifying_glass.php
+++ d02/ex02/magnifying_glass.php
@@ -1,18 +1,24 @@
 #!/usr/bin/php
+
+function	
 <?php
 	if ($argc == 2 && is_readable($argv[1])) {
-		$content = file_get_contents($argv[1]);
-		$content = preg_replace_callback('/(<a )(.*)(>)(.*)(<\/a>)/i', function($matches) {
-			$matches[0] = preg_replace_callback('/( title=\")(.*?)(\")/i', function ($matches) {
-						return ($matches[1]."".strtoupper($matches[2])."".$matches[3]);
-					},
-					$matches[0]);
-			$matches[0] = preg_replace_callback("/(>)(.*?)(<)/si", function($matches) {
-				return ($matches[1]."".strtoupper($matches[2])."".$matches[3]);
-				}, $matches[0]);
-		
-			return ($matches[0]);
+		$content = file_get_contents($argv[1]) or exit(1);
+		$content = preg_replace_callback('/(<a )(.*)(>)(.*)(<\/a>)/is', function($magnify) {
+			// echo "1:\n" . print_r($magnify);
+			// print_r($magnify);
+			$magnify[0] = preg_replace_callback('/( title=\")(.*?)(\")/im', function($magnify) {
+						// echo "2:\n";
+						// print_r($magnify);
+						return ($magnify[1].strtoupper($magnify[2]).$magnify[3]);
+					}, $magnify[0]);
+			$magnify[0] = preg_replace_callback("/(>)(.*?)(<)/s", function($magnify) {
+				// echo "3:\n";
+				// print_r($magnify);
+				return ($magnify[1].strtoupper($magnify[2]).$magnify[3]);
+				}, $magnify[0]);
+			return ($magnify[0]);
 		}, $content);
-		echo ($content);
+		echo $content . "\n";
 	}
 ?>
